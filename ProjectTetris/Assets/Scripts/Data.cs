bbc9fcc85dc32a7c599c5804ccafc41817533cf5
Richard <108534431+IAMRichardKeith@users.noreply.github.com>
2022-07-26 17:07:35 -0400

    Assigned cell values to each Tetris piece

--- ProjectTetris/Assets/Scripts/Data.cs
+++ ProjectTetris/Assets/Scripts/Data.cs
@@ -2,17 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Data : MonoBehaviour
+//CODE IS NOT MINE. Followed along from a Tutorial Video
+//THIS CODE DOES NOT BELONG TO ME
+public static class Data
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public static readonly float cos = Mathf.Cos(Mathf.PI / 2f);
+    public static readonly float sin = Mathf.Sin(Mathf.PI / 2f);
+    public static readonly float[] RotationMatrix = new float[] { cos, sin, -sin, cos };
+
+    //A dictionary is a key value pairing. The dictionary is used for storing value pairs in EACH element instead of single values.
+    //These are the positions we will assign to the specific Tetris pieces
+    public static readonly Dictionary<Tetriminos, Vector2Int[]> Cells = new Dictionary<Tetriminos, Vector2Int[]>()
     {
-        
-    }
+        //The vector 2 int values are the associated values for the specific Tetris pieces to be drawn in the specific cells of the grid
+        { Tetriminos.IPiece, new Vector2Int[] { new Vector2Int(-1, 1), new Vector2Int( 0, 1), new Vector2Int( 1, 1), new Vector2Int( 2, 1) } },
+        { Tetriminos.JPiece, new Vector2Int[] { new Vector2Int(-1, 1), new Vector2Int(-1, 0), new Vector2Int( 0, 0), new Vector2Int( 1, 0) } },
+        { Tetriminos.LPiece, new Vector2Int[] { new Vector2Int( 1, 1), new Vector2Int(-1, 0), new Vector2Int( 0, 0), new Vector2Int( 1, 0) } },
+        { Tetriminos.OPiece, new Vector2Int[] { new Vector2Int( 0, 1), new Vector2Int( 1, 1), new Vector2Int( 0, 0), new Vector2Int( 1, 0) } },
+        { Tetriminos.SPiece, new Vector2Int[] { new Vector2Int( 0, 1), new Vector2Int( 1, 1), new Vector2Int(-1, 0), new Vector2Int( 0, 0) } },
+        { Tetriminos.TPiece, new Vector2Int[] { new Vector2Int( 0, 1), new Vector2Int(-1, 0), new Vector2Int( 0, 0), new Vector2Int( 1, 0) } },
+        { Tetriminos.ZPiece, new Vector2Int[] { new Vector2Int(-1, 1), new Vector2Int( 0, 1), new Vector2Int( 0, 0), new Vector2Int( 1, 0) } },
+    };
+
+    private static readonly Vector2Int[,] WallKicksI = new Vector2Int[,] {
+        { new Vector2Int(0, 0), new Vector2Int(-2, 0), new Vector2Int( 1, 0), new Vector2Int(-2,-1), new Vector2Int( 1, 2) },
+        { new Vector2Int(0, 0), new Vector2Int( 2, 0), new Vector2Int(-1, 0), new Vector2Int( 2, 1), new Vector2Int(-1,-2) },
+        { new Vector2Int(0, 0), new Vector2Int(-1, 0), new Vector2Int( 2, 0), new Vector2Int(-1, 2), new Vector2Int( 2,-1) },
+        { new Vector2Int(0, 0), new Vector2Int( 1, 0), new Vector2Int(-2, 0), new Vector2Int( 1,-2), new Vector2Int(-2, 1) },
+        { new Vector2Int(0, 0), new Vector2Int( 2, 0), new Vector2Int(-1, 0), new Vector2Int( 2, 1), new Vector2Int(-1,-2) },
+        { new Vector2Int(0, 0), new Vector2Int(-2, 0), new Vector2Int( 1, 0), new Vector2Int(-2,-1), new Vector2Int( 1, 2) },
+        { new Vector2Int(0, 0), new Vector2Int( 1, 0), new Vector2Int(-2, 0), new Vector2Int( 1,-2), new Vector2Int(-2, 1) },
+        { new Vector2Int(0, 0), new Vector2Int(-1, 0), new Vector2Int( 2, 0), new Vector2Int(-1, 2), new Vector2Int( 2,-1) },
+    };
+
+    private static readonly Vector2Int[,] WallKicksJLOSTZ = new Vector2Int[,] {
+        { new Vector2Int(0, 0), new Vector2Int(-1, 0), new Vector2Int(-1, 1), new Vector2Int(0,-2), new Vector2Int(-1,-2) },
+        { new Vector2Int(0, 0), new Vector2Int( 1, 0), new Vector2Int( 1,-1), new Vector2Int(0, 2), new Vector2Int( 1, 2) },
+        { new Vector2Int(0, 0), new Vector2Int( 1, 0), new Vector2Int( 1,-1), new Vector2Int(0, 2), new Vector2Int( 1, 2) },
+        { new Vector2Int(0, 0), new Vector2Int(-1, 0), new Vector2Int(-1, 1), new Vector2Int(0,-2), new Vector2Int(-1,-2) },
+        { new Vector2Int(0, 0), new Vector2Int( 1, 0), new Vector2Int( 1, 1), new Vector2Int(0,-2), new Vector2Int( 1,-2) },
+        { new Vector2Int(0, 0), new Vector2Int(-1, 0), new Vector2Int(-1,-1), new Vector2Int(0, 2), new Vector2Int(-1, 2) },
+        { new Vector2Int(0, 0), new Vector2Int(-1, 0), new Vector2Int(-1,-1), new Vector2Int(0, 2), new Vector2Int(-1, 2) },
+        { new Vector2Int(0, 0), new Vector2Int( 1, 0), new Vector2Int( 1, 1), new Vector2Int(0,-2), new Vector2Int( 1,-2) },
+    };
+
+    public static readonly Dictionary<Tetriminos, Vector2Int[,]> WallKicks = new Dictionary<Tetriminos, Vector2Int[,]>()
+    {
+        { Tetriminos.IPiece, WallKicksI },
+        { Tetriminos.JPiece, WallKicksJLOSTZ },
+        { Tetriminos.LPiece, WallKicksJLOSTZ },
+        { Tetriminos.OPiece, WallKicksJLOSTZ },
+        { Tetriminos.SPiece, WallKicksJLOSTZ },
+        { Tetriminos.TPiece, WallKicksJLOSTZ },
+        { Tetriminos.ZPiece, WallKicksJLOSTZ },
+    };
 }
+
--- ProjectTetris/Assets/Scripts/Tetriminos.cs
+++ ProjectTetris/Assets/Scripts/Tetriminos.cs
@@ -20,6 +20,13 @@ public struct TetriminoData
 {
     public Tetriminos tetriminos;
     public Tile tile;
-    public Vector2Int[] cells;
+    public Vector2Int[] cells {get; private set;} //<-- This property allows the cells data to not be viewable on the Unity Editor.
+
+
+    public void Initialize()
+    {
+        //Access the data class and cells property. We are now looking up the cells associated with the tetromino data
+        this.cells = Data.Cells[this.tetriminos] //Assigning our cells to the tetrimino data
+    }
 
 }
